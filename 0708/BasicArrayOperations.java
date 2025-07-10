eb5e60d9ebaed75d47aa7dc6f9e2d5e896bb0526
412631292 <f40432392@gmail.com>
2025-07-10 11:45:31 +0800

    0708

--- 0708/BasicArrayOperations.java
+++ 0708/BasicArrayOperations.java
@@ -1,19 +1,12 @@
-public class ArrayAccess {
-public static void main(String[] args) {
-    int[]score={85,92,77,60,88};
-    int first=score[0];//取第一位
-    int second=score[3];
-    int third=score[2];
-    System.out.println(first);
-    System.out.println(second);
-    System.out.println(third);
-    
-    score[0]=90;//改第一個成績
-    score[score.length-1]=95;//改最後一個
 
-    int Arraylenth=score.length;//陣列長度
-    System.out.println("改後");
-    for (int i = 0; i < Arraylenth; i++) {
-        System.out.printf("索引 %d:%d",i,score[i]);
-        
+public class BasicArrayOperations {
+    public static void main(String[] args) {
+        int[] numbers = {15, 28, 7, 42, 91, 33, 66, 58, 24, 81};
+        System.out.println("陣列長度: " + numbers.length);
+        numbers[2] = 99;
+        numbers[numbers.length - 1] = 100;
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(i + ": " + numbers[i]);
+        }
     }
+}   
